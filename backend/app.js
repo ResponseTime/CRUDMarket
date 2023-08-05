098398d17907607d89796df02bf91679269fb4f2
Looper <aayush.bhattacharjee2002@gmail.com>
2023-08-05 13:28:32 +0530

    Started working on backend

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,29 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+let db = require("./db.js");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ ok: 1 });
+app.get("/products", async (req, res) => {
+  let col = await db.collection("products");
+  let data = await col.find();
+  let js = [];
+  for await (let col of data) {
+    json = {
+      ProductName: col.productName,
+      Price: col.price,
+      PostedBy: col.postedby,
+    };
+    js.push(json);
+  }
+  res.json(js);
 });
+app.post("/product/add", (req, res) => {});
+app.delete("/product/:id", (req, res) => {});
+app.put("/product/:id", (req, res) => {});
+
 app.listen(5000, () => {
   console.log("running");
 });
---
+++ backend/db.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+let uri = process.env.MONGO_URI;
+let client = new MongoClient(uri);
+client.connect((err) => {
+  throw err;
+});
+let db = client.db("Project");
+
+module.exports = db;
